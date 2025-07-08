5b3059ba8758558153d982ce266ceeea0254fed3
Stephanie <stephyborrego04@gmail.com>
2025-07-08 09:37:39 -0600

    ajuste al yml

---
+++ app.js
@@ -0,0 +1,7 @@
+const moment = require("moment");
+let i = 5;
+let j = 10;
+console.log('Iniciando...');
+console.log("La suma es "+(i+j));
+let fecha= moment().format('LL');
+console.log("y la fecha es: ", fecha);
